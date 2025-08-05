92efe574c7fb1775aa6fde71cb57e2b3b90d48d2
Marcos Suarez <suarezmarcos298@gmail.com>
2025-08-05 19:03:23 -0300

    ejercicios 05/08/25

---
+++ carrito/src/DataFixtures/ProductoFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use app\Entity\Producto;
+class ProductoFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i < 10; $i++) {
+        $producto = new Producto();
+        $producto->setNombre('producto '.$i);
+        $producto->setDescripcion('este es el producto numero '.$i);
+        $producto->setPrecio(mt_rand(10, 100));
+        $producto->setImagen('images/producto'.$i.'.jpg');
+        $manager->persist($producto);
+        }  
+        $manager->flush();
+    }
+}
